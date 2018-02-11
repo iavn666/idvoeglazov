436d5ef4de58789a78346c4f14d777bf19baca6e
iavan666 <dvoeglazovcom@gmail.com>
2018-02-11 14:38:24 +0500

    1.4. JUnit

--- chapter_001/scr/main/java/ru/job4j/Calculate.java
+++ chapter_001/scr/main/java/ru/job4j/Calculate.java
@@ -1,16 +1,31 @@
 package ru.job4j;
 
 /**
-
-* @author ivan dvoeglazov (ivan.vanya.2012@inbox.ru)
-
-*/
+ * Calculate.
+ *
+ * @author ivan dvoeglazov (ivan.vanya.2012@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class Calculate {
 
-public static void main	(String[] args) {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
-   System.out.println ("Hello world");
+     /**
+      * Method echo.
+      * @param name Your name.
+      * @return Echo plus your name.
+      */
+
+      public String echo(String name) {
+           return "Eho, echo, echo : " + name;
 }
 
 }      
