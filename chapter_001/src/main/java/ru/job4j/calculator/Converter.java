64fcb98b7a21848244da8a34b5b506f13c3ccd6f
iavan666 <dvoeglazovcom@gmail.com>
2018-02-17 19:47:30 +0500

    3.2.1 Конвертер валюты.

--- chapter_001/src/main/java/ru/job4j/calculator/Converter.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Converter.java
@@ -1,7 +1,11 @@
 package ru.job4j.calculator;
 
 /**
- * Корвертор валюты.
+ * Конвертер валют
+ *
+ * @author Dvoeglazov Ivan (ivan.vanya.2012@inbox.ru)
+ * @version 1
+ * @since 0.1
  */
 public class Converter {
 
@@ -13,25 +17,56 @@ public class Converter {
     /**
      * Обьявляем значения переменных
      */
-    int Euro = 70;
-    int Dollar = 60;
-    int rubli;
+    int euro = 70;
+    int dollar = 60;
+    int value;
 
-    public int rubleToEuro(int rubli) {
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро
+     */
+
+    public int rubleToEuro(int value) {
         /**
          * Арифметическая операция.
          */
-        result = rubli / Euro ;
-        return -1;
+         return value / euro;
     }
     /**
      * Конвертируем рубли в доллары.
      * @param value рубли.
      * @return Доллоры
      */
-    public int rubleToDollar(int value) {
-        result = Dollar / Euro ;
-        return -1;
+        public int rubleToDollar(int value) {
+            /**
+             * Арифметическая операция.
+             */
+        return value / dollar;
     }
+    /**
+     * Конвертируем доллары в рубли.
+     * @param value Доллоры.
+     * @return рубли.
+     */
+
+    public int DollarTorubli(int value) {
+        /**
+         * Арифметическая операция.
+         */
+        return  dollar * value;
+    }
+    /**
+     * Конвертируем евро в рубли.
+     * @param value евро.
+     * @return рубли.
+     */
+    public int EuroTorubli(int value) {
+        /**
+         * Арифметическая операция.
+         */
+        return euro * value;
+    }
+
 
 }
--- chapter_001/src/test/java/ru/job4j/calculator/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -4,18 +4,54 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Тестирует методы класса Converter
+ *
+ * @author Dvoeglazov Ivan (ivan.vanya.2012@inbox.ru)
+ * @version 1
+ * @since 0.1
+ */
+
 public class ConverterTest {
+    /**
+     * Тестирует метод rubleToDollar().
+     */
     @Test
-    public void when60RubleToDollarThen1() {
+        public void when60RubleToDollarThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToDollar(60);
-        assertThat(result, is(1D));
+        int result = converter.rubleToDollar(120);
+        assertThat(result, is(2));
     }
 
+    /**
+     * Тестирует метод rubleToDollar().
+     */
+    @Test
+    public void RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(140);
+        assertThat(result, is(2));
+    }
+
+    /**
+     * Тестирует метод DollarTorubli().
+     */
+    @Test
+    public void whenEuroTorubli() {
+        Converter converter = new Converter();
+        int result = converter.DollarTorubli(1);
+        assertThat(result, is(60));
+    }
+
+    /**
+     * Тестирует метод EuroTorubli().
+     */
     @Test
     public void when70RubleToEuroThen1() {
         Converter converter = new Converter();
-        int result = converter.rubleToDollar(70);
-        assertThat(result, is(1D));
+        int result = converter.EuroTorubli(1);
+        assertThat(result, is(70));
     }
+
+
 }
