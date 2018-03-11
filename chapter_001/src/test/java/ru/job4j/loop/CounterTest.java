1ae05ee46cc0f687922876916d0fbe147322cc6f
iavan666 <dvoeglazovcom@gmail.com>
2018-03-11 21:24:36 +0500

    4.1.1 Подсчет суммы чётных чисел в диапазоне [#192]

--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -5,14 +5,36 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * @author ivan Dvoeglazov (ivan.vanya.2012@inbox.ru)
+ * @version 1
+ * @since 0.1
+ */
+
+
 public class CounterTest {
     @Test
+    /**
+     * Тестирует метод whenSumEvenNumbersFromOneToTenThenThirty.
+     */
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        /*
+         * Создаем переменную count со ссылкой на обьект Counter,  до этого ранее созданный
+        */
         Counter count = new Counter();
-        count.add(1,10);
-        double expected = 30D;
-        assertThat(count.add(1,10), is(expected));
-
+        /*
+        *Объявляем переменную result ссылаясь через переменную count на обьект Counter и на его метод add
+        * вносим тестируемые значения
+         */
+        int result = count.add(1,10);
+        /*
+        *Объявляем переменную expected тоесть ожидаемое значение которое должны получить по результату теста
+         */
+        int expected = 30;
+        /*
+        *Сравниваем полученное значение от метода add c ожидаемым
+         */
+        assertThat(result, is(expected));
 
         //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове метода counter.add будет равна 30.
     }
