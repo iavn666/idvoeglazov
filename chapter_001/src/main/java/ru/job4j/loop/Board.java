ee64cc2841eb9b38989c47e03eb7fc8c901fb2ca
iavan666 <dvoeglazovcom@gmail.com>
2018-03-12 19:35:21 +0500

    4.3. Построить шахматную доску в псевдографике. [#13559]

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -1,11 +1,29 @@
 package ru.job4j.loop;
 
+/**
+ * @author ivan Dvoeglazov (ivan.vanya.2012@inbox.ru)
+ * @version 1
+ * @since 12.03.2018
+ */
+
 public class Board {
+    /*
+    *Объявляем метод paint, указываем ему две переменные width и height
+     */
     public String paint(int width, int height) {
+        /*
+        *Создаем переменную screen со сылкой на обьект StringBuilder позваляет вставлять символы в строку
+         */
         StringBuilder screen = new StringBuilder();
+        /*
+        * Разделяет строки
+         */
         String ln = System.lineSeparator();
-        for (int a = height; a<6;a++) {
-            for (int b = width; b<6;b++) {
+        /*
+        * Обьявляем цикл в условии обьявляем переменную (а) которая будет меньше переменной height
+         */
+        for (int a = 0;a<height; a++) {
+            for (int b = 0;b<width;b++){
                 // условие проверки, что писать пробел или X
                 // Выше в задании мы определили закономерность, когда нужно проставлять X
                 if ((a + b) % 2 == 0) {
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -18,9 +18,9 @@ public class BoardTest {
     @Test
     public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
         Board board = new Board();
-        String result = board.paint(5, 4);
+        String result = board.paint( 5,4);
         final String line = System.getProperty("line.separator");
-        String expected = String.format("x x%s x %sx x%s x ", line, line, line, line);
+        String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line);
         assertThat(result, is(expected));
 
         //напишите здесь тест, проверяющий формирование доски 5 на 4.
