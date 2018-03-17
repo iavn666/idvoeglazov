ea683d858a0f6a99f9948752d94e2f6f508d9353
iavan666 <dvoeglazovcom@gmail.com>
2018-03-17 18:14:04 +0500

    5.0. Заполнить массив степенями чисел. [#33488][#44746]

--- chapter_001/src/main/java/ru/array/Square.java
+++ chapter_001/src/main/java/ru/array/Square.java
@@ -9,27 +9,11 @@ public class Square {
 
     public int[] calculate(int bound) {
 
-        /*
-        * Обьявили массив с длинной bound
-         */
 
         int[] rst = new int[bound];
-        rst [0] = 1;
-        rst [1] = 2;
-        rst [2] = 3;
-        rst [3] = 4;
-        rst [4] = 5;
-        rst [5] = 6;
 
-        /*
-        * Цикл с условием и переменнай i, тагже указали что длинна массива (rst.length) будет больше
-        * переменной i
-         */
-        for (int i = 0; rst.length > i; i++) {
-            /*
-            *Возводим переменную i в квадрат через Math.pow
-             */
-            rst[i] = (int) Math.pow(rst[i], 2);
+              for (int i = 0; rst.length > i; i++) {
+              rst[i] = (int) Math.pow(rst[i], 2);
                     }
                   return rst;
 
