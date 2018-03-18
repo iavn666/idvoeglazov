7f6ac592856d86759a0f814b403b88ec499e0ab1
iavan666 <dvoeglazovcom@gmail.com>
2018-03-18 22:01:48 +0500

    5.6. Удаление дубликатов в массиве. [#225]

--- chapter_001/src/main/java/ru/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/array/ArrayDuplicate.java
@@ -1,15 +1,26 @@
 package ru.array;
+/**
+ * @author ivan Dvoeglazov (ivan.vanya.2012@inbox.ru)
+ * @version 1
+ * @since 18.03.2018
+ */
 
 import java.util.Arrays;
 
-public class ArrayDuplicate {
 
+
+public class ArrayDuplicate {
+    /**
+     * find duplicated array
+     * @param array
+     * @return duplicated array
+     */
     public String[] remove(String[] array){
         int uni = array.length;
-        for(int out = 0;out<array.length;out++){
-            for (int in = out+1;in<array.length;in++){
+        for(int out = 0;out<uni;out++){
+            for (int in = out+1;in<uni;in++){
                 if(array[out].equals(array[in])){
-                    array[in]=array[uni - 1];
+                    array[in] = array[uni-1];
                     uni--;
                     in--;
                 }
--- chapter_001/src/test/java/ru/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/array/ArrayDuplicateTest.java
@@ -1,5 +1,11 @@
 package ru.array;
 
+/**
+ * @author ivan Dvoeglazov (ivan.vanya.2012@inbox.ru)
+ * @version 1
+ * @since 18.03.2018
+ */
+
 import org.junit.Test;
 
 import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
@@ -10,6 +16,16 @@ public class ArrayDuplicateTest {
     public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
         String [] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
         String [] except = {"Привет", "Мир", "Супер"};
+        ArrayDuplicate a = new ArrayDuplicate();
+        String [] result = a.remove(input);
+        assertThat(result,arrayContainingInAnyOrder(except));
+        //напишите здесь тест, проверяющий удаление дубликатов строк из массива строк.
+    }
+    @Test
+    public void whenRemoveThenArrayWithoutDuplicate() {
+
+        String [] input = {"1", "1"};
+        String [] except = {"1",};
         ArrayDuplicate a = new ArrayDuplicate();
         String [] result = a.remove(input);
         assertThat(result,arrayContainingInAnyOrder(except));
